e64eb51ab92e37723c9930f06063c0e0a113329f
Zoxooly <zentegondi@gmail.com>
2026-09-19 13:32:34 +0200

    Implement password check on submit button click
    Add password validation functionality with access response.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let button = document.getElementById("secretButton");
 button.onclick = function() {
     alert("test works");
 };
+
+let password = document.getElementById("password");
+let submit = document.getElementById("submit");
+let response = document.getElementById("response");
+
+submit.onclick = function() {
+    if (password.value == "THERMOSA") {
+        response.textContent = "ACCESS GRANTED.";
+    } else {
+        response.textContent = "ACCESS DENIED.";
+    }
+};
